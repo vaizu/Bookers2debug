749ed825d927176095c46184a12d66258ed16e7c
EC2 Default User <ec2-user@ip-172-31-22-173.ec2.internal>
2022-01-28 12:43:42 +0000

    Bookers2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+
+  def index
+    @book = Book.new
+    @users = User.all
+  end
+
+  def edit
+    @book = Book.new
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user.id)
+    end
+  end
+
+  def update
+    @book = Book.new
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+  has_one_attached :profile_image
+
+  validates :name, presence: true,
+          length: { minimum: 2, maximum: 20 }
+
+         validates :name, uniqueness: true
+  validates :name, presence: true
+   validates :introduction, presence: true
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+  validates :introduction, presence: false,length: { maximum: 50 }
+end
